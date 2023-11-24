fb5e48296afe8f2925904ed3c646ba5e469fef81
Lorenzo3212 <mavet3212@gmaul.com>
2023-11-23 22:57:07 -0500

    primer commit

---
+++ Operadores.py
@@ -0,0 +1,52 @@
+# Esto es un comentario
+print('Hola Mundo')
+
+print(type('hola'))
+print(type(4))
+print(type(1.5))
+
+a = 4
+b = 6
+c = a + b
+print(c)
+
+numero = c
+print('Mi numero favorito es:' , numero ) # Concatenar
+print("Mi numero favorito es:" , numero )
+
+# Operadores
+
+print(6 + 3) #suma
+print(6 - 3) #resta
+print(6 * 3) #multiplicación
+print(6 / 3) #División
+print(8 % 3) #Resto (divide el numero y el resto que sobra es lo que arroja de regreso)
+print(3 ** 3) #Exponente
+print(8 // 3) #Divisor de Flor (cuantas veces entra el numero y el resto lo elimina)
+
+a = 6
+print('hola ' + 'como estas')
+print('hola ' * 6)
+print('hola ' + str(a))
+
+#Operadores comparativos
+
+print(4 < 8)
+print(4 > 8)
+print(4 == 8)
+print(4 <= 8)
+print(4 >= 8)
+print(4 != 8) #Desigual o diferente
+
+
+print(len('hola') > len('rolas')) #cuenta los caracteres
+
+
+#Operadores Logicos
+ 
+print(4 < 6 | 7 > 9) # and
+print(4 < 6 | 7 > 9) # Or
+print(not(4 > 6 )) # cambia la condición si es falso lo convierte e verdadero y biceversa
+
+
+print((not(7 != 7) and 6 > 5) and ('rozar' < 'rotar' or not(3 == 5)))
